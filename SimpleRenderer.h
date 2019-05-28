4a3985261de81cf7450e963ee6c08c6a7ec5620d
Paul Rouget <me@paulrouget.com>
2019-05-28 18:02:08 +0200

    Servo loads, but fails on init egl. Trying to get stdout

--- SimpleRenderer.h
+++ SimpleRenderer.h
@@ -13,21 +13,7 @@ namespace foobar
         void UpdateWindowSize(GLsizei width, GLsizei height);
 
     private:
-        GLuint mProgram;
         GLsizei mWindowWidth;
         GLsizei mWindowHeight;
-
-        GLint mPositionAttribLocation;
-        GLint mColorAttribLocation;
-
-        GLint mModelUniformLocation;
-        GLint mViewUniformLocation;
-        GLint mProjUniformLocation;
-
-        GLuint mVertexPositionBuffer;
-        GLuint mVertexColorBuffer;
-        GLuint mIndexBuffer;
-
-        int mDrawCount;
     };
 }
